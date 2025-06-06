84dda11c7cd38767a97a755a107e27c2ddb62574
Michael Phillips <mphillips242001@outlook.com>
2025-06-06 15:31:38 -0500

    added guestList module

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
-
+import { guestHTML } from "./guestList.js"
 
 
 
@@ -8,13 +8,14 @@ const applicationHTML = `
 <h1>Cider Falls River Trails</h1>
 <article class="details">
         <section class="detail--column list details__">
-        
+        <h1>Guest List</h1>
+        ${guestHTML()}
         </section>
         <section class="detail--column list details__">
         
         </section>
 
-
+    </article>
     `
 
     mainContainer.innerHTML = applicationHTML
