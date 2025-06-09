ddcf8623fb7f1f6d23df330a017b3c3c99e4f44c
Stephen <peterson.stephen138@gmail.com>
2025-06-09 09:52:41 -0500

    Revert "Added Park Module"

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,12 @@
 import { guestHTML } from "./guestList.js"
 import { serviceHTML } from "./servicesList.js"
-import { parkHTML } from "./parkAreas.js"
+
 
 const mainContainer = document.querySelector("#container")
 
 const applicationHTML = `
 <h1>Cider Falls River Trails</h1>
 <article class="details">
-        <section class="detail--column list details__">
-        <h1>Parks</h1>
-        ${parkHTML()}
         <section class="detail--column list details__">
         <h1>Guest List</h1>
         ${guestHTML()}
